89ec85212880c78adce82efdc925fc4f91f092be
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2018-01-31 12:01:26 +0100

    Modul 3 - JavaScript

---
+++ Modul 3 - JavaScript/lektion-7/exempel-8.js
@@ -0,0 +1,10 @@
+// Skapa en funktion med en parameter
+function hej(msg){
+    // Funktionen hej skickar ett meddelane
+    // vidare till JavaScript-metoden alert
+    alert(msg);
+
+    // Ingen return behövs här!
+    return 0; // onödigt
+}
+
